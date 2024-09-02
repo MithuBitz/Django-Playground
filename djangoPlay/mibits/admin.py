04690cca9ea895e8985e78df486ee58e34573575
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-09-03 01:08:05 +0530

    Establish model relation like One2One, One2Manu and Many2Many

--- djangoPlay/mibits/admin.py
+++ djangoPlay/mibits/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
-from .models import NamesList
+from .models import NamesList, NameReview, DevCommunity, DevCertificate
 
 # Register your models here.
-admin.site.register(NamesList)
+class NameReviewInline(admin.TabularInline):
+    model = NameReview
+    extra = 2
+
+class NameListAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'add_time')
+    inlines = [NameReviewInline]
+
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'language')
+    filter_horizontal = ('name_lists',)
+
+class DevCerficateAdmin(admin.ModelAdmin):
+    list_display = ('dev_name', 'certificate_number')
+
+admin.site.register(NamesList, NameListAdmin)
+admin.site.register(DevCommunity, CommunityAdmin)
+admin.site.register(DevCertificate, DevCerficateAdmin)
+
--- djangoPlay/mibits/models.py
+++ djangoPlay/mibits/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 NAME_TYPE_CHOICE = [
         ("MB", "MITHU"),
@@ -19,3 +20,33 @@ class NamesList(models.Model):
 
     def __str__(self):
         return self.name
+
+# One to Many relation between models
+class NameReview(models.Model):
+    review_name = models.ForeignKey(NamesList, on_delete=models.CASCADE, related_name='reviews') 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) :
+        return f'{self.user.username} review for {self.review_name.name}'
+    
+# Many to Many relationship
+class DevCommunity(models.Model):
+    name = models.CharField(max_length=100)
+    language = models.CharField(max_length=40)
+    name_lists = models.ManyToManyField(NamesList, related_name='devcomm')
+
+    def __str__(self) :
+        return self.name
+    
+# One to One relation
+class DevCertificate(models.Model):
+    dev_name = models.OneToOneField(NamesList, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateTimeField()
+
+    def __str__(self) :
+        return f'Certificate for {self.name.dev_name}'
